7a7b9e5ca0fab2c642b0dc1899f05c5fa031731b
Sergey <llbarmenll@ya.ru>
2020-03-29 16:58:02 +0300

    Update cron_releases.sh

--- cron_releases.sh
+++ cron_releases.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 for i in $(seq 1 5); do
-  /opt/bin/python3 /opt/etc/movies/digitalreleases2.py > /opt/etc/movies/log.txt
+  /opt/bin/python3 /opt/etc/movies/digitalreleasesv3.py > /etc/movies/log.txt
   ret=$?
   if [ $ret -eq 0 ]; then
-    logger -t "digitalreleases.py" "Загрузка завершена успешно."
+    logger -t "digitalreleasesv3.py" "Загрузка завершена успешно."
     break
   else
-    logger -t "digitalreleases.py" "Ошибка загрузки."
+    logger -t "digitalreleasesv3.py" "Ошибка загрузки."
   fi
 done
